0963cbe9e42a613a0a37abdbbb2c0b8987e04f4a
Sbiaa <developer.biaa@gmail.com>
2026-07-08 18:40:31 -0300

    v2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -14,7 +14,7 @@ export async function fetchApi(path: string, options: RequestInit = {}) {
     (headers as Record<string, string>)["Content-Type"] = "application/json";
   }
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${path}`, {
+  const res = await fetch(`/api${path}`, {
     ...options,
     credentials: "include",
     headers,
